3f6b531d6e581bbf9c7b812593da68ce2299b6de
Gustavo Batista <59751350+batistag395@users.noreply.github.com>
2022-03-10 15:22:18 -0300

    finalizando funçao de editar/ salvar ediçao

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,12 +10,15 @@ import { RemoveBtnComponent } from '../remove-btn/remove-btn.component';
 })
 export class PainelComponent implements OnInit {
 
+  
+  public tarefa: "";
+  
+  public items: Array<Task> = [{text: 'item 1'}];
+  
+  public findItem: number = 2;
+  
   public indexItem = new EventEmitter();
-
-  public findItem: number = 0
-
-  public items: Task[] = [{text: 'item 1'}];
-
+  
   adiciona(tarefa: Task){
 
     this.items.unshift(tarefa)
@@ -26,23 +29,35 @@ export class PainelComponent implements OnInit {
   
   remove(tarefa: Task){
 
-    this.items.splice( this.items.indexOf(tarefa), 1 )
+    this.items.splice( this.items.indexOf(tarefa), 1)
+
+    this.indexItem.emit(this.items);
   }
 
   editTarefa(tarefa : Task)
   {
     this.findItem = this.items.indexOf(tarefa);
+    console.log("findItem "+this.findItem)
+    console.log(tarefa)
   }
 
   saveTarefa(edition : string)
   {
-    this.items[this.findItem].text = edition;
+    this.items[this.findItem].text = edition
+
     this.indexItem.emit(this.items);
+
+    console.log("edition " + edition)
   }
   
   constructor() { }
 
   ngOnInit(): void {
+    let dado = this;
+    setTimeout(function () {
+      dado.indexItem.emit(dado.items);
+    }, 1000);
+
   }
 
 }
--- src/app/remove-btn/remove-btn.component.ts
+++ src/app/remove-btn/remove-btn.component.ts
@@ -10,17 +10,24 @@ import { Task } from '../task.interface';
 })
 export class RemoveBtnComponent implements OnInit {
 
-  @Input() indexItem = new EventEmitter()
+  @Input() 
+  indexItem = new EventEmitter()
 
-  @Input() tarefa: Task
+  @Input() tarefa: Task;
 
+  @Input() items: Task[];
+  
   @Output() update = new EventEmitter<object>();
 
-  @Input() items: Task[]
+  @Output() remove: EventEmitter<any> = new EventEmitter();
 
-  @Output() remove: EventEmitter<any> = new EventEmitter()
+  @Output() editItem =new EventEmitter();
 
-  public editionItem : string = "";
+  @Output() saveItem= new EventEmitter();
+
+  public editionItem: string;
+
+  public isEditing : Boolean = false;
 
   removeTarefa(tarefa: Task){
 
@@ -34,30 +41,33 @@ export class RemoveBtnComponent implements OnInit {
  
   }
 
-
-  @Output() editItem =new EventEmitter();
-  
-  @Output() saveItem= new EventEmitter();
-
-  public isEditing : Boolean = false;
-
-  editTarefa(tarefa: Task)
+  editTask(tarefa: Task)
   {
     this.isEditing = true;
+
     this.editItem.emit(tarefa)
   }
 
   saveTarefa(edition : string)
   {
-    if (!edition) return;
+    //if (!edition) return;
 
-    this.saveItem.emit(edition);
     this.isEditing = false;
+    
+    this.saveItem.emit(edition);
   }
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    if (this.indexItem) {
+      this.indexItem.subscribe((setDados) => {
+
+        this.items = setDados; 
+      });
+    }
+    
+  }
+  
 }
